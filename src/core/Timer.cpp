996bd029710d07f797e7553358fe6d42a307e6fe
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-06-23 18:11:53 +0200

    Fix Timer bug not showing minutes

--- src/core/Timer.cpp
+++ src/core/Timer.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <stdint.h>
 #include <core/Timer.h>
-
+#include <stdio.h>
 void Core::Timer::init() {
     m_frames = 0;
 
@@ -23,10 +23,11 @@ void Core::Timer::update() {
 
     uint32_t centsec = ((uint32_t) m_time) % 100;
 
-    uint32_t sec = (((uint32_t) m_time) / 100) % 60;
-    uint32_t min = (sec / 60) % 60;
+    uint32_t sec = (((uint32_t) m_time) / 100);
+    uint32_t min = sec / 60;
+    sec = sec % 60;
 
-    //printf("%02d:%02d:%02d\n", min, sec, centsec);
+    printf("%02d:%02d:%02d\n", min, sec, centsec);
 
     str[0] = min / 10;
     str[1] = min % 10;
