27b256f51322d6348c96df4feef31ee5aeb11daa
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-06-26 17:34:59 +0200

    Add input test to dummy target

--- src/targets/dummy/SettingsImpl.cpp
+++ src/targets/dummy/SettingsImpl.cpp
@@ -3,18 +3,32 @@
 #include "SettingsImpl.h"
 
 void SettingsImpl::initGraphics() {
-    input_test_str.initGraphics();
-    exit_str.initGraphics();
+    input_msg.initGraphics();
+
+    for (unsigned int i = 0; i < SettingsState::SETTINGS_NB; i++) {
+        settings_str[i].initGraphics();
+    }
+
+    for (unsigned int i = 0; i < 8; i++) {
+        inputs_str[i].initGraphics();
+        input_states_str[i].initGraphics();
+    }
 }
 
 void SettingsImpl::draw() const {
     switch (m_state) {
         case SettingsState::LIST:
-            input_test_str.draw();
-            exit_str.draw();
+            for (unsigned int i = 0; i < SettingsState::SETTINGS_NB; i++) {
+                settings_str[i].draw();
+            }
             break;
 
         case SettingsState::INPUT_TEST:
+            input_msg.draw();
+            for (unsigned int i = 0; i < 8; i++) {
+                inputs_str[i].draw();
+                input_states_str[i].draw();
+            }
             break;
     }
 }
