d3cb14ffee1303aafcac6f31eb352765db4af254
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-06-28 18:06:32 +0200

    Change main filenam

---
+++ src/core/main.cpp
@@ -0,0 +1,45 @@
+/* open-tgm.cpp */
+
+#include <ctime>
+#include <stdint.h>
+#include <Global.h>
+#include <Input.h>
+#include <Menu.h>
+#include <Game.h>
+#include <Background.h>
+#include <Player.h>
+#include <Stack.h>
+#include <Frame.h>
+#include <App.h>
+
+
+uint32_t rand_seed;
+uint8_t stack_position;
+
+Player player1;
+Stack stack1;
+Frame frame1;
+
+#ifdef MULTIPLAYER
+Player player2;
+Stack stack2;
+Frame frame2;
+#endif
+
+Input input;
+Game game;
+Menu menu;
+Background background;
+
+int main() {
+    // Init game stuff
+    init();
+
+    // Init graphical assets
+    initGraphics();
+
+    // Run game
+    app();
+
+    return 0;
+}
