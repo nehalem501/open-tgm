d1d930fe01687a65f9c9ba1bd95ccfb22a1cb248
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-07-07 16:41:00 +0200

    PSP target buils correctly now

--- src/targets/psp/AppImpl.cpp
+++ src/targets/psp/AppImpl.cpp
@@ -1,7 +1,35 @@
 /* AppImpl.cpp - PSP */
 
+#include <pspkernel.h>
+#include <pspdebug.h>
+#include <pspdisplay.h>
+#include <pspctrl.h>
+#include "resources/callbacks.h"
+#include <Global.h>
+#include <Menu.h>
+#include <Background.h>
 #include <App.h>
 
-void app() {
+#define VERS 1
+#define REVS 0
 
+PSP_MODULE_INFO("Open TGM", PSP_MODULE_USER, VERS, REVS);
+PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
+PSP_HEAP_SIZE_MAX();
+
+#define printf pspDebugScreenPrintf
+
+void app() {
+    setupCallbacks();
+
+    while (running()) {
+        // Do frame
+        menu.update();
+        background.draw();
+        menu.draw();
+
+        // TODO VBLANK
+    }
+
+    sceKernelExitGame();
 }
