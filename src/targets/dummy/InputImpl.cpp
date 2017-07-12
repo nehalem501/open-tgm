7281d501474cee83ac12c5e044cd0f8cc083fa0e
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-07-12 16:37:14 +0200

    Add instructions for input in dummy target

--- src/targets/dummy/InputImpl.cpp
+++ src/targets/dummy/InputImpl.cpp
@@ -6,4 +6,19 @@ void InputImpl::init() {
 }
 
 void InputImpl::pollInputs() {
+    // Read and store inputs
+
+    /*
+    m_up = BUTTON_UP;
+    m_down = BUTTON_DOWN;
+    m_left = BUTTON_LEFT;
+    m_right = BUTTON_RIGHT;
+
+    m_a = BUTTON_A;
+    m_b = BUTTON_B;
+    m_c = BUTTON_C;
+
+    m_start = BUTTON_START;
+    m_settings = BUTTON_SELECT;
+    */
 }
