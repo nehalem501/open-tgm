290e70b9c8c94cae6ff13b5b019397722c536472
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-07-12 16:41:17 +0200

    PSP Input analog stick comment

--- src/targets/psp/InputImpl.cpp
+++ src/targets/psp/InputImpl.cpp
@@ -10,6 +10,10 @@ void InputImpl::init() {
 void InputImpl::pollInputs() {
     sceCtrlReadBufferPositive(&m_pad, 1);
 
+    // TODO Read analog stick
+    //m_pad.Lx
+    //m_pad.Ly
+
     m_up = m_pad.Buttons & PSP_CTRL_UP;
     m_down = m_pad.Buttons & PSP_CTRL_DOWN;
     m_left = m_pad.Buttons & PSP_CTRL_LEFT;
