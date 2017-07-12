12428be81a9964dc78d8d94b98f28a18433de5fd
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-07-12 16:35:12 +0200

    Add input for PSP

--- src/targets/psp/InputImpl.cpp
+++ src/targets/psp/InputImpl.cpp
@@ -3,7 +3,25 @@
 #include "InputImpl.h"
 
 void InputImpl::init() {
+    sceCtrlSetSamplingCycle(0);
+    sceCtrlSetSamplingMode(PSP_CTRL_MODE_ANALOG);
 }
 
 void InputImpl::pollInputs() {
+    sceCtrlReadBufferPositive(&m_pad, 1);
+
+    m_up = m_pad.Buttons & PSP_CTRL_UP;
+    m_down = m_pad.Buttons & PSP_CTRL_DOWN;
+    m_left = m_pad.Buttons & PSP_CTRL_LEFT;
+    m_right = m_pad.Buttons & PSP_CTRL_RIGHT;
+
+    m_a = (m_pad.Buttons & PSP_CTRL_SQUARE)
+          || (m_pad.Buttons & PSP_CTRL_LTRIGGER);
+    m_b = (m_pad.Buttons & PSP_CTRL_TRIANGLE)
+          || (m_pad.Buttons & PSP_CTRL_CROSS)
+          || (m_pad.Buttons & PSP_CTRL_RTRIGGER);
+    m_c = m_pad.Buttons & PSP_CTRL_CIRCLE;
+
+    m_start = m_pad.Buttons & PSP_CTRL_START;
+    m_settings = m_pad.Buttons & PSP_CTRL_SELECT;
 }
--- src/targets/psp/InputImpl.h
+++ src/targets/psp/InputImpl.h
@@ -3,12 +3,15 @@
 #ifndef INPUT_PSP_H
 #define INPUT_PSP_H
 
+#include <pspctrl.h>
 #include <core/Input.h>
 
 class InputImpl : public Core::Input {
     public:
         void init();
         void pollInputs();
+
+        SceCtrlData m_pad;
 };
 
 #endif
