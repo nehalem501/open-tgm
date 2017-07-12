79025508fb2a1250871f6dc58f5823057a5e9366
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-07-12 16:35:48 +0200

    Add background for PSP

--- src/targets/psp/BackgroundImpl.cpp
+++ src/targets/psp/BackgroundImpl.cpp
@@ -1,14 +1,41 @@
 /* BackgroundImpl.cpp - PSP */
 
+#include <stdlib.h>
+#include <glib2d.h>
 #include "BackgroundImpl.h"
 
 void BackgroundImpl::initGraphics() {
+    m_texture = g2dTexLoad("resources/background.png", G2D_SWIZZLE);
 }
 
 void BackgroundImpl::draw() const {
     if (options) {
         // Draw settings background
+        g2dBeginQuads(NULL);
+
+        g2dSetColor(G2D_RGBA(0, 0, 0xFF, 0xFF));
+        g2dSetCoordXY(0, 0);
+        g2dAdd();
+        g2dSetCoordXY(G2D_SCR_W, 0);
+        g2dAdd();
+
+        g2dSetColor(BLACK);
+        g2dSetCoordXY(G2D_SCR_W, G2D_SCR_H);
+        g2dAdd();
+        g2dSetCoordXY(0, G2D_SCR_H);
+        g2dAdd();
+
+        g2dEnd();
     } else {
         // Draw game background
+        g2dBeginRects(m_texture);
+
+        if (m_texture == NULL)
+            g2dSetColor(RED);
+
+        g2dSetCoordXY(0,0);
+        g2dAdd();
+
+        g2dEnd();
     }
 }
--- src/targets/psp/BackgroundImpl.h
+++ src/targets/psp/BackgroundImpl.h
@@ -3,12 +3,16 @@
 #ifndef BACKGROUND_PSP_H
 #define BACKGROUND_PSP_H
 
+#include <glib2d.h>
 #include <core/Background.h>
 
 class BackgroundImpl : public Core::Background {
     public:
         void initGraphics();
         void draw() const;
+
+    private:
+        g2dTexture *m_texture;
 };
 
 #endif
