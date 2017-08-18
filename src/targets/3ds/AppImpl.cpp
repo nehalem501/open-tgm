c14ee68af014a2ea14436feec367eba0e9ca11d6
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-08-18 18:41:51 +0200

    Minimal code for 3DS target

--- src/targets/3ds/AppImpl.cpp
+++ src/targets/3ds/AppImpl.cpp
@@ -1,7 +1,49 @@
 /* AppImpl.cpp - 3DS */
 
+#include <3ds.h>
+#include <citro3d.h>
+#include <Global.h>
+#include <Menu.h>
+#include <Background.h>
 #include <App.h>
 
-void app() {
+#define DISPLAY_TRANSFER_FLAGS \
+	(GX_TRANSFER_FLIP_VERT(0) | GX_TRANSFER_OUT_TILED(0) | GX_TRANSFER_RAW_COPY(0) | \
+	GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGB8) | \
+    GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
 
+#define TEXTURE_TRANSFER_FLAGS \
+	(GX_TRANSFER_FLIP_VERT(1) | GX_TRANSFER_OUT_TILED(1) | GX_TRANSFER_RAW_COPY(0) | \
+	GX_TRANSFER_IN_FORMAT(GX_TRANSFER_FMT_RGBA8) | GX_TRANSFER_OUT_FORMAT(GX_TRANSFER_FMT_RGBA8) | \
+	GX_TRANSFER_SCALING(GX_TRANSFER_SCALE_NO))
+
+C3D_RenderTarget* target;
+
+void app() {
+    gfxInitDefault();
+    C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+
+    // Initialize the render target
+    target = C3D_RenderTargetCreate(240, 400, GPU_RB_RGBA8, GPU_RB_DEPTH24_STENCIL8);
+    C3D_RenderTargetSetClear(target, C3D_CLEAR_ALL, 0x000000FF, 0);
+    C3D_RenderTargetSetOutput(target, GFX_TOP, GFX_LEFT, DISPLAY_TRANSFER_FLAGS);
+
+    // Main loop
+    while (aptMainLoop()) {
+        hidScanInput(); // Refresh Inputs
+
+        // Process one frame
+        menu.update();
+
+        // Draw
+        C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+		C3D_FrameDrawOn(target);
+        background.draw();
+        menu.draw();
+		C3D_FrameEnd(0);
+    }
+
+	// Deinitialize graphics
+    C3D_Fini();
+    gfxExit();
 }
