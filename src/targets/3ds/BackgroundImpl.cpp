c2e0b3997f30b4d50a8f800232bd293481d8810b
Tomek LECOCQ <tomek.lecocq@hotmail.fr>
2017-08-25 10:39:07 +0200

    Add settings background in 3DS target

--- src/targets/3ds/BackgroundImpl.cpp
+++ src/targets/3ds/BackgroundImpl.cpp
@@ -11,10 +11,29 @@ void BackgroundImpl::initGraphics() {
 void BackgroundImpl::draw() const {
     if (options) {
         // Draw settings background
+        C3D_TexBind(0, &empty_tex);
+        C3D_ImmDrawBegin(GPU_TRIANGLE_STRIP);
+
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.5f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.0f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 1.0f, 1.0f);
+
+        C3D_ImmSendAttrib(0.0f, 240.0f, 0.5f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.0f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.0f, 0.0f);
+
+        C3D_ImmSendAttrib(400.0f, 0.0f, 0.5f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.0f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 1.0f, 1.0f);
+
+        C3D_ImmSendAttrib(400.0f, 240.0f, 0.5f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.0f, 0.0f);
+        C3D_ImmSendAttrib(0.0f, 0.0f, 0.0f, 0.0f);
+
+        C3D_ImmDrawEnd();
     } else {
         // Draw game background
         C3D_TexBind(0, &background_tex);
-
         C3D_ImmDrawBegin(GPU_TRIANGLE_STRIP);
 
         C3D_ImmSendAttrib(0.0f, 0.0f, 0.5f, 0.0f);
