5cac7d2034c123d1d71a1fbbf8974ac96ddce4cd
nehalem501 <tomek.lecocq@hotmail.fr>
2018-02-19 09:22:29 +0100

    PSP build

--- psp.mk
+++ psp.mk
@@ -1,14 +1,14 @@
 TARGET = open-tgm
-SOURCE_FILES_CPP= $(wildcard core/*.cpp) $(wildcard targets/psp/*.cpp) $(wildcard modes/*.cpp)
-SOURCE_FILES_C= $(wildcard targets/psp/resources/*.c)
+SOURCE_FILES_CPP= $(wildcard src/core/*.cpp) $(wildcard src/targets/psp/*.cpp) $(wildcard src/modes/*.cpp)
+SOURCE_FILES_C= $(wildcard src/targets/psp/resources/*.c)
 OBJS = $(SOURCE_FILES_C:.c=.o) $(SOURCE_FILES_CPP:.cpp=.o)
 
-INCDIR = ./include ./targets/psp ../extlibs/gLib2D
+INCDIR = ./src/include ./src/targets/psp ./extlibs/gLib2D
 CFLAGS = -O2 -G0 -Wall -DTARGET_PSP -DDEBUG
 CXXFLAGS = $(CFLAGS) -fno-exceptions -fno-rtti
 ASFLAGS = $(CFLAGS)
 
-LIBDIR = ../extlibs/gLib2D
+LIBDIR = ./extlibs/gLib2D
 LDFLAGS =
 LIBS= -lglib2d -ljpeg -lpng -lz -lpspgum -lpspgu -lpsprtc -lm -lpspvram
 
@@ -22,9 +22,9 @@ logo.o: logo.raw
 	bin2o -i logo.raw logo.o logo
 
 finalize:
-	mkdir -p ../build/psp
-	mv EBOOT.PBP ../build/psp
-	rm -rf ../build/psp/resources
-	cp -r ../data/platform_specific/psp ../build/psp
-	mv ../build/psp/psp ../build/psp/resources
+	mkdir -p ./build/psp
+	mv EBOOT.PBP ./build/psp
+	rm -rf ./build/psp/resources
+	cp -r ./data/platform_specific/psp ./build/psp
+	mv ./build/psp/psp ./build/psp/resources
 
