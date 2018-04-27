793063b91ea22c5ff2c73e163d10e7ec1698754f
nehalem501 <tomek.lecocq@hotmail.fr>
2018-04-27 20:27:59 +0200

    Put GBA 'press start' at correct position

--- src/targets/gba/HomeImpl.cpp
+++ src/targets/gba/HomeImpl.cpp
@@ -4,8 +4,8 @@
 
 void HomeImpl::init_graphics() {
     m_start.init_graphics();
-    m_start.update_text("PRESS START");
-    m_start.update_pos(0, 0);
+    m_start.update_text("PRESS  START");
+    m_start.update_pos(9, 12);
     m_start.update_graphics();
 }
 
