b7d40b9941cdfd12a38d7951ac86bca525edb759
BuildTools <tomek.lecocq@hotmail.fr>
2018-04-27 16:23:16 +0200

    fix GBA tilemap

--- src/targets/gba/GlobalImpl.cpp
+++ src/targets/gba/GlobalImpl.cpp
@@ -2,9 +2,7 @@
 
 #include <cstring>
 #include <gba.h>
-#include <resources/tilemap.h>
-#include <resources/outline.h>
-#include <resources/shared.h>
+#include <resources/fulltilemap.h>
 #include <Global.h>
 
 void init_random() {
@@ -19,11 +17,6 @@ void init_textures() {
     REG_BG1CNT = TILE_BASE(0) | MAP_BASE(16) | BG_256_COLOR | TEXTBG_SIZE_256x256;
 
     // Load tilemap and it's palette into VRAM
-    dmaCopy(tilemapTiles, TILE_BASE_ADR(0), tilemapTilesLen);
-    dmaCopy(sharedPal, BG_PALETTE, sharedPalLen);
-
-    dmaCopy(outlineTiles, TILE_BASE_ADR(1), outlineTilesLen);
-    //dmaCopy(outline_pal, BG_PALETTE + TILEMAP_PAL_LEN,  OUTLINE_PAL_LEN);
-    /* We need the same palette for all tilemaps */
-    /* TODO palette generation taking into acount both tilemaps */
+    dmaCopy(fulltilemapTiles, TILE_BASE_ADR(0), fulltilemapTilesLen);
+    dmaCopy(fulltilemapPal, BG_PALETTE, fulltilemapPalLen);
 }
---
+++ src/targets/gba/resources/fulltilemap.h
@@ -0,0 +1,29 @@
+
+//{{BLOCK(fulltilemap)
+
+//======================================================================
+//
+//	fulltilemap, 8x2776@8, 
+//	Transparent color : 00,FF,FF
+//	+ palette 256 entries, not compressed
+//	+ 347 tiles not compressed
+//	Total size: 512 + 22208 = 22720
+//
+//	Time-stamp: 2018-04-27, 16:17:11
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.12
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_FULLTILEMAP_H
+#define GRIT_FULLTILEMAP_H
+
+#define fulltilemapTilesLen 22208
+extern const unsigned short fulltilemapTiles[11104];
+
+#define fulltilemapPalLen 512
+extern const unsigned short fulltilemapPal[256];
+
+#endif // GRIT_FULLTILEMAP_H
+
+//}}BLOCK(fulltilemap)
