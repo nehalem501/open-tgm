9eb4fa8b737150ac68a225fe172a67ad1decadcf
nehalem501 <tomek.lecocq@hotmail.fr>
2018-04-27 20:27:06 +0200

    Implement GBA input

--- src/targets/gba/InputImpl.cpp
+++ src/targets/gba/InputImpl.cpp
@@ -1,5 +1,6 @@
 /* InputImpl.cpp - GBA */
 
+#include <gba.h>
 #include "InputImpl.h"
 
 void InputImpl::init() {
@@ -13,26 +14,26 @@ void InputImpl::poll_inputs() {
     m_curr_buttons = 0x00;
 
     // Joystick
-    /*if (up)
+    if (~(REG_KEYINPUT) & KEY_UP)
         m_curr_joystick |= UP_BIT | RAW_UP_BIT;
-    if (down)
+    if (~(REG_KEYINPUT) & KEY_DOWN)
         m_curr_joystick |= DOWN_BIT | RAW_DOWN_BIT;
-    if (left)
+    if (~(REG_KEYINPUT) & KEY_LEFT)
         m_curr_joystick |= LEFT_BIT | RAW_LEFT_BIT;
-    if (right)
-        m_curr_joystick |= RIGHT_BIT | RAW_RIGHT_BIT;*/
+    if (~(REG_KEYINPUT) & KEY_RIGHT)
+        m_curr_joystick |= RIGHT_BIT | RAW_RIGHT_BIT;
 
     // Buttons
-    /*if (a)
+    if (~(REG_KEYINPUT) & KEY_B)
         m_curr_buttons |= A_BIT;
-    if (b)
+    if (~(REG_KEYINPUT) & KEY_A)
         m_curr_buttons |= B_BIT;
-    if (c)
+    if (~(REG_KEYINPUT) & KEY_R)
         m_curr_buttons |= C_BIT;
-    if (start)
+    if (~(REG_KEYINPUT) & KEY_START)
         m_curr_buttons |= START_BIT;
-    if (settings)
-        m_curr_buttons |= SETTINGS_BIT;*/
+    if (~(REG_KEYINPUT) & KEY_SELECT)
+        m_curr_buttons |= SETTINGS_BIT;
 
     process();
 }
