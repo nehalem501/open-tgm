1286179f7a5b7684c84d294db2100c230900cb64
nehalem501 <tomek.lecocq@hotmail.fr>
2018-05-04 16:53:32 +0200

    GBA Target Player implementation

--- src/targets/gba/PlayerImpl.cpp
+++ src/targets/gba/PlayerImpl.cpp
@@ -1,5 +1,8 @@
 /* PlayerImpl.cpp - GBA */
 
+#include <gba.h>
+#include <Shapes.h>
+#include <Piece.h>
 #include "PlayerImpl.h"
 
 void PlayerImpl::init_graphics() {
@@ -13,9 +16,21 @@ void PlayerImpl::draw() const {
         // Draw ghost piece
     }
 
-    if (m_draw_piece) {
+    //if (m_draw_piece) {
         // Draw player piece
-    }
+        u16 *local_screen = (u16*) MAP_BASE_ADR(16);
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                //x = pos_x + (m_piece.pos_x() - 2 + i) * tile_size
+                //y = pos_y + (m_piece.pos_y() - 1 + j) * tile_size);
+
+                local_screen[32 * (m_piece.pos_y() - 1 + j) + (m_piece.pos_x() - 2 + i)] = 5;
+            }
+        }
+        //for (int i = 0; i < m_height - 2; i++) {
+        //    memcpy(local_screen + 32 * i, m_field + m_width * (i + 2), m_width * 2);
+        //}
+    //}
 
     // Draw next piece
 
--- src/targets/gba/StackImpl.cpp
+++ src/targets/gba/StackImpl.cpp
@@ -11,18 +11,18 @@ void StackImpl::update_graphics() {
 
 void StackImpl::draw() const {
     // Draw stack blocks and outline
-    /*#define m_pos_x 0
+    //#define m_pos_x 0
     u16 *local_screen = (u16*) MAP_BASE_ADR(16) + m_pos_x;
     for (int i = 0; i < m_height - 2; i++) {
-        memcpy(local_screen + 32 * i, m_field[m_width * (i + 2)], m_width * 2);
+        memcpy(local_screen + 32 * i, m_field + m_width * (i + 2), m_width * 2);
     }
 
     local_screen = (u16*) MAP_BASE_ADR(24) + m_pos_x;
     for (int i = 0; i < m_height - 2; i++) {
-        memcpy(local_screen + 32 * i, m_outline[m_width * (i + 2)], m_width * 2);
+        memcpy(local_screen + 32 * i, m_outline + m_width * (i + 2), m_width * 2);
     }
 
     for (unsigned int i = 0; i < FILLED_LINES_NB; i++) {
         m_part[i].draw();
-    }*/
+    }
 }
