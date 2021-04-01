443c05ac50d5025999b631ea4944b5e0c4ebea6a
Tomek Lecocq <tomek.lecocq@hotmail.fr>
2021-04-01 11:30:11 +0200

    Fix OpenGL backend compilation

--- src/platforms/sdl2/OpenGL1Dispatch.h
+++ src/platforms/sdl2/OpenGL1Dispatch.h
@@ -3,12 +3,13 @@
 #ifndef OPENGL1DISPATCH_SDL2_H
 #define OPENGL1DISPATCH_SDL2_H
 
+#ifdef __APPLE__
 // We need this for modern macOS
 #define GL_SILENCE_DEPRECATION
+#endif
 
+// TODO
 #include <SDL_opengl.h>
-
-
 
 #ifdef __APPLE__
 #include <OpenGL/gl.h>
