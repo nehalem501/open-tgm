0ddaf060a78480455e0a37527a2acdaa9246a664
nehalem501 <nehalem501@users.noreply.github.com>
2021-11-28 10:09:58 +0100

    Add dummy GPU backend

--- src/gpu/backends/dummy/GPUTypes.h
+++ src/gpu/backends/dummy/GPUTypes.h
@@ -3,8 +3,12 @@
 #ifndef GPU_TYPES_DUMMY_GPU_H
 #define GPU_TYPES_DUMMY_GPU_H
 
-typedef float gpu_float_t;
-typedef unsigned int gpu_indices_t;
+#include <stdint.h>
+
+typedef float gpu_coord_t;
+typedef float gpu_texcoord_t;
+typedef float gpu_color_t;
+typedef uint16_t gpu_indices_t;
 typedef unsigned int texture_t;
 
 #endif // GPU_TYPES_DUMMY_GPU_H
