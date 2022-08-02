0ddd2a084bf173545f6d0af541e2c77cb431de4c
nehalem501 <tomek.lecocq@hotmail.fr>
2022-08-02 13:33:53 +0200

    Remove old logging methods

--- src/platforms/dummy/GlobalImpl.cpp
+++ src/platforms/dummy/GlobalImpl.cpp
@@ -6,29 +6,8 @@
 #include <Debug.h>
 #include <stdio.h>
 
-base_dout& base_dout::operator << (const char* str) {
-    printf("%s", str);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const bool value) {
-    operator<<(value ? "true" : "false");
-    return *this;
-}
-
-base_dout& base_dout::operator << (const int value) {
-    printf("%d", value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const size_t value) {
-    operator<<((int) value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const uint32_t value) {
-    operator<<((int)  value);
-    return *this;
+void printd_str(const char *str) {
+    fputs(str, stdout);
 }
 #endif
 
--- src/platforms/dummy_gpu/GlobalImpl.cpp
+++ src/platforms/dummy_gpu/GlobalImpl.cpp
@@ -6,44 +6,8 @@
 #include <Debug.h>
 #include <stdio.h>
 
-base_dout& base_dout::operator << (const char* str) {
-    fprintf(stderr, "%s", str);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const bool value) {
-    operator<<(value ? "true" : "false");
-    return *this;
-}
-
-base_dout& base_dout::operator << (const short value) {
-    operator<<((int) value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const int value) {
-    fprintf(stderr, "%d", value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const long value) {
-    operator<<((int) value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const unsigned short value) {
-    operator<<((int) value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const unsigned int value) {
-    operator<<((int) value);
-    return *this;
-}
-
-base_dout& base_dout::operator << (const unsigned long value) {
-    operator<<((int) value);
-    return *this;
+void printd_str(const char *str) {
+    fputs(str, stdout);
 }
 #endif
 
