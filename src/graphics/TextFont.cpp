59a8587d7ca1327ef9e5986fcca5960b499d7654
Nehalem501 <tomek.lecocq@hotmail.fr>
2025-11-23 20:10:42 +0100

    Fix segfault in graphics/TextFont.cpp

--- src/graphics/TextFont.cpp
+++ src/graphics/TextFont.cpp
@@ -70,34 +70,44 @@ static Buffer draw_char(FT_Face face, char c) {
     FT_Bitmap bitmap = glyph->bitmap;
     uint8_t* data = new uint8_t[bitmap.rows * bitmap.width];
     //std::vector<uint8_t> data;
+    //std::cout << "size: " << bitmap.rows * bitmap.width << std::endl;
+    //int size = 0;
     for (unsigned int y = 0; y < bitmap.rows; y++) {
         //std::cout << "y: " << y << ", pitch: " << bitmap.pitch << std::endl;
         for (unsigned int byte_index = 0; byte_index < (unsigned int) bitmap.pitch; byte_index++) {
             uint8_t byte_value = bitmap.buffer[y * bitmap.pitch + byte_index];
             //std::bitset<8> byte_value(bitmap.buffer[y * bitmap.pitch + byte_index]);
-            //std::cout << "  i: " << byte_index << ", " << std::bitset<8>(byte_value) << std::endl;
+            //std::cout << "byte_index: " << byte_index << std::endl;
             //std::cout << std::bitset<8>(byte_value);
             //std::cout << "index: " << byte_index << ", width: " << bitmap.width << std::endl;
-            unsigned int num_bits_done = byte_index * 8;
+            int num_bits_done = byte_index * 8;
             unsigned int rowstart = y * bitmap.width + byte_index * 8;
-            uint8_t max_index = std::min(8u, bitmap.width - num_bits_done);
-            for (uint8_t bit_index = 0; bit_index < max_index; bit_index++) {
+            int max_index = std::min(8, (int) bitmap.width - num_bits_done);
+            for (uint8_t bit_index = 0; (int) bit_index < max_index; bit_index++) {
                 uint8_t bit = byte_value & (1 << (7 - bit_index));
                 //std::cout << (bit ? "#" : ".");
+                //size++;
+                /*if (rowstart + bit_index >= bitmap.rows * bitmap.width) {
+                    std::cout << "oops: rowstart: " << rowstart << ", bit_index: " << (int) bit_index << ", total: " << rowstart + bit_index << std::endl;
+                    std::cout << "  bitmap.width: " << bitmap.width << ", num_bits_done: " << num_bits_done << ", max_index: " << (int) max_index << std::endl;
+                }**/
                 data[rowstart + bit_index] = bit ? 1 : 0;
                 //data.push_back(bit ? 1 : 0);
             }
         }
         //std::cout << std::endl;
     }
+    //std::cout << "size: " << size << std::endl;
 
     unsigned int width = bitmap.width;
     unsigned int height = bitmap.rows;
 
     //std::cout << "char: " << c << ", width: " << width << ", height: " << height << std::endl;
 
+    //size = 0;
     for (unsigned int y = 0; y < height; y++) {
         for (unsigned int x = 0; x < width; x++) {
+            //size++;
             if (data[y * width + x])
                 std::cout << '#';
             else
@@ -105,11 +115,12 @@ static Buffer draw_char(FT_Face face, char c) {
         }
         std::cout << std::endl;
     }
+    //std::cout << "size: " << size << std::endl;
     std::cout << std::endl;
     delete[] data;
 
-    int size = 1;
-    canvas_ity::canvas context(size, size);
+    //int size = 16;
+    //canvas_ity::canvas context(size, size);
 
     /*context.set_color(canvas_ity::fill_style, 0, 0, 0, 0);
     context.fill_rectangle(0, 0, size, size);
@@ -119,11 +130,12 @@ static Buffer draw_char(FT_Face face, char c) {
 
     //context.
 
-    const size_t image_size = size * size * 4;
-    uint8_t *image = new uint8_t[image_size];
-    context.get_image_data(image, size, size, size * 4, 0, 0);
+    //const size_t image_size = size * size * 4;
+    //uint8_t *image = new uint8_t[image_size];
+    //context.get_image_data(image, size, size, size * 4, 0, 0);
 
-    return Buffer { image, image_size };
+    //return Buffer { image, image_size };
+    return Buffer { NULL, 0 };
 }
 
 void generate_text_font(float current_tile_size/*, std::function<void(const GeneratedTexture&)> callback*/) {
@@ -158,7 +170,7 @@ void generate_text_font(float current_tile_size/*, std::function<void(const Gene
         texture_pixels = texture_width * texture_height;
     } while (texture_pixels > min_size);
 
-    canvas_ity::canvas context(texture_width, texture_height);
+    //canvas_ity::canvas context(texture_width, texture_height);
 
     //float s = round(current_tile_size / 8.0f);
 
@@ -172,9 +184,9 @@ void generate_text_font(float current_tile_size/*, std::function<void(const Gene
         draw_char(face, chars_to_render[i]);
     }
 
-    size_t texture_size = texture_width * texture_height * 4;
-    uint8_t *texture = new uint8_t[texture_size];
-    context.get_image_data(texture, texture_width, texture_height, texture_width * 4, 0, 0);
+    //size_t texture_size = texture_width * texture_height * 4;
+    //uint8_t *texture = new uint8_t[texture_size];
+    //context.get_image_data(texture, texture_width, texture_height, texture_width * 4, 0, 0);
     //Buffer b = { texture, texture_size };
     //GeneratedTexture t = { b, texture_width, texture_height };
     //callback(t);
@@ -186,7 +198,7 @@ void generate_text_font(float current_tile_size/*, std::function<void(const Gene
     std::ofstream stream( filename, std::ios::binary );
     stream.write( reinterpret_cast< char * >( header ), sizeof( header ) );
     stream.write( reinterpret_cast< char * >( texture ), texture_height * texture_width * 4 );*/
-    delete[] texture;
+    //delete[] texture;
 }
 
 /*int main() {
